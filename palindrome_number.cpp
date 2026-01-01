ba279f85d09a25a700c6037839b3d942ce085922
vishalkv21 <vishalkv2107@gmail.com>
2026-01-01 16:24:11 +0530

    Solved Palindrome number

---
+++ palindrome_number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0)
+        {
+            return false;
+        }
+        string s=to_string(x);
+        int i=0,j=s.size()-1;
+        while (i<j)
+        {
+            if (s[i]==s[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
